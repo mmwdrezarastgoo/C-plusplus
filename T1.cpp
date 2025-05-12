f22c4e40664c215244eb6db2d2428c07d0f5d7b7
mmwdrezarastgoo <mr.rezarastgoo1381@gmail.com>
2025-05-12 05:49:59 -0700

    Add files via upload

---
+++ T1.cpp
@@ -0,0 +1,19 @@
+//t1
+#include <iostream>
+#include <conio.h>
+using namespace std;
+ main()
+{
+	cout<<"Hello"<<"ali"<<endl;
+	
+	cout<<"Hello"<<'\t'<<"ali"<<endl<<endl;
+	
+	cout<<"Hello"<<'\n'<<"ali"<<endl;
+	cout<<"Hello"<<endl<<"ali"<<endl<<endl;
+	
+	cout<<"Hello"<<'\b'<<"ali"<<endl;
+	cout<<"Hello"<<'\b';
+	
+	cout<<'\a';
+	getch();
+}
