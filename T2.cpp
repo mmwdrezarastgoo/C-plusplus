b87f5af3f9a7cbf93906391b95aecc8bd9cc9c5a
mmwdrezarastgoo <mr.rezarastgoo1381@gmail.com>
2025-05-12 06:27:55 -0700

    Add files via upload

---
+++ T2.cpp
@@ -0,0 +1,14 @@
+//t2= getting  a single integer number from input and show twise those number in output
+#include <iostream>
+#include <conio.h>
+using namespace std;
+main()
+{
+	int a; //range of int :  -32768  to  +32768    (assuming that 2 bite assigne for int (ps: some system assigne 4 bite))
+	cout<<"(please enter a integer number as a)"<<endl<<"a=";
+	cin>>a; //By using    cin    we can receive a thing in output 
+	cout<<"2*a=";
+	cout<<2*a; //using    cout    without " "  we can calculate
+		
+		getch();
+}
