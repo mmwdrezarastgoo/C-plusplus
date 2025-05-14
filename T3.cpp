ad258a3de1315644ce2b4c3e1390f5c8610198ad
mmwdrezarastgoo <mr.rezarastgoo1381@gmail.com>
2025-05-14 07:10:25 -0700

    Add files via upload

---
+++ T3.cpp
@@ -0,0 +1,16 @@
+//t3:getting two integer numbers from input and calculate summation of those two numbers in output
+#include <iostream>
+#include <conio.h>
+using namespace std;
+main()
+{
+	int a,b;
+	cout<<"enter a integer number as a :"<<endl;
+	cin>>a;
+	cout<<"enter another integer number as b :"<<endl;
+	cin>>b;
+	cout<<"------------------------------------"<<endl;
+	cout<<"a+b="<<a+b;
+	
+	getch();
+}
